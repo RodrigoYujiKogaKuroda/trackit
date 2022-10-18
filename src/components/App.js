05cbb95faaba4d5506a3830cc9d01e6e62f52094
AiKurogaya <yujikuroda12@hotmail.com>
2022-10-18 11:30:25 -0300

    feat: Added Main route

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from '../theme/globalStyle';
 
+import MainPage from "./mainPage/MainPage";
+
 export default function App() {
 
     return (
         <>
         <GlobalStyle />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+            </Routes>
+        </BrowserRouter>
         </>
     );
 
---
+++ src/components/mainPage/MainPage.js
@@ -0,0 +1,11 @@
+import LOGO from './../img/logo.png'
+
+export default function MainPage() {
+
+    return (
+        <>
+            <img src={LOGO} alt="logo"></img>
+        </>
+    );
+
+}
